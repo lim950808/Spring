5824fbd3cf9cd290151865e925dd5a35eb96e7f1
lim950808 <lim080895@gmail.com>
2022-01-05 21:43:56 +0900

    och03_DI

---
+++ och03_DI/src/main/java/DI04/CollectionBean.java
@@ -0,0 +1,16 @@
+package DI04;
+
+import java.util.Map;
+
+public class CollectionBean {
+	private Map<String, String> addressList;
+
+	public Map<String, String> getAddressList() {
+		return addressList;
+	}
+
+	public void setAddressList(Map<String, String> addressList) {
+		this.addressList = addressList;
+	}
+	
+}
